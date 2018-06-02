c956fb5ab095489935f5887094fc9f99c61925f4
Yeojin Kim <yeojin.dev@gmail.com>
2018-06-02 10:58:31 +0900

    Add UIECtrl.cs
    스와이프에 따른 Template 컨트롤 코드 추가

---
+++ Assets/Scripts/UIECtrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DoozyUI;
+
+public class UIECtrl : MonoBehaviour {
+
+    public UIElement[] uiElements;
+
+    public Image roadBar;
+
+    private int index = 0;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void ShowNextUIE()
+    {
+        uiElements[index].Hide(false);  // true: 즉시, false: 애니메이션 처리
+        index = (index + 1) > uiElements.Length - 1 ? 0 : index + 1;
+        uiElements[index].Show(false);
+    }
+
+    public void ShowPrevUIE()
+    {
+        uiElements[index].Hide(false);
+        index = (index - 1) < 0 ? uiElements.Length - 1 : index - 1;
+        uiElements[index].Show(false);
+    }
+}
